2c99353f0de22dae0b922e597d12d095e71f3cc4
Harry Kennedy <harry.s.kennedy2007@gmail.com>
2025-09-29 16:55:27 +0100

    Create Ship.java

---
+++ Ship.java
@@ -0,0 +1,16 @@
+public class Ship {
+  
+ 
+        int size;
+        int position;
+        boolean isHorizontal;
+       
+       
+       BattleShip( int size, int position, boolean isHorizontal){
+       this.size = size;
+       this.position = position;
+       this.isHorizontal = isHorizontal;
+       }
+      
+      }//end of class
+}
