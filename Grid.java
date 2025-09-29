19d3dc79b8c793caf818cbd7c9e0f8a2afc7d3ee
ArnavMK <kothariarnav9@gmail.com>
2025-09-29 17:42:28 +0100

    Initialize Grid class

--- Grid.java
+++ Grid.java
@@ -7,17 +7,21 @@ public class Grid {
         
         initialize();
 
+    }
+    
+    private void initialize() {
+         
         for (int row = 0; row < 10; row++) {
             for (int col = 0; col < 10; col++) {
+
+                 
                 gridCoordinates[row][col] = new GridUnit(row, col);
             }
         }
-    }
-    
-    public void initialize() {
-         
-    } 
 
+
+    } 
+    
     public GridUnit[][] getGrid() {
         return gridCoordinates; 
     }
@@ -28,6 +32,7 @@ class GridUnit {
     private int x;
     private int y;
     private boolean isUnderShip;
+    private Ship ship;
     
     public GridUnit(int x, int y) {
         this.x = x;
